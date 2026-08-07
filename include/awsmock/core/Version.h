ee4e1b5e588d6eb04b4e8ed5f453285ca8b8c6a4
jensv <jens.vogt@opitz-consulting.com>
2026-08-07 15:36:29 +0200

    fix: new nodesJS shin stderr/stdeout handling

--- include/awsmock/core/Version.h
+++ include/awsmock/core/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define APP_VERSION "0.1.11"
+#define APP_VERSION "0.1.12"
 #define APP_VERSION_MAJOR 0
 #define APP_VERSION_MINOR 1
-#define APP_VERSION_PATCH 11
+#define APP_VERSION_PATCH 12
